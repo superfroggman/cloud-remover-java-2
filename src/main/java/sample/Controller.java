e55c8413fd78d4d0b0c53c3225909b825cb691eb
superfroggman <49985901+superfroggman@users.noreply.github.com>
2020-05-24 22:23:56 +0200

    swapped to javaCV and gitignored libraries

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,4 +1,10 @@
 package sample;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+
+    public void buttonClicked(ActionEvent actionEvent) {
+        System.out.println("button clicked");
+    }
 }
