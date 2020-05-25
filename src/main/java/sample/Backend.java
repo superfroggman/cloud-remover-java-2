f0afffb25834c695df9596e5d8343eb09bb13b81
superfroggman <49985901+superfroggman@users.noreply.github.com>
2020-05-25 10:10:02 +0200

    started doing cloud removing

--- src/main/java/sample/Backend.java
+++ src/main/java/sample/Backend.java
@@ -1,8 +1,10 @@
 package sample;
 
 import org.bytedeco.javacv.*;
+import org.bytedeco.javacv.Frame;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 
@@ -21,7 +23,7 @@ public class Backend {
     public static void customizedCopyPastedThing() throws IOException {
 
         //set temp inputs and outputs
-        String input = "/home/anton/Downloads/tutorial.mp4";
+        String input = "/home/anton/Downloads/mackan.mp4";
         String output = "/home/anton/Downloads/images";
 
         //start things
@@ -42,20 +44,48 @@ public class Backend {
         frame = frameGrabber.grabKeyFrame();
         BufferedImage baseImage = bimConverter.convert(frame);
 
+        int width = baseImage.getWidth();
+        int height = baseImage.getHeight();
+
         //Loop through all frames
-        for (int i = 0; i < nFrames; i++) {
+        for (int i = 1; i < nFrames - 1; i++) {
+
+            System.out.println("Starting frame: " + i);
 
             //Set currentImage to i:th image
             frameGrabber.setFrameNumber(i);
             frame = frameGrabber.grabKeyFrame();
             BufferedImage currentImage = bimConverter.convert(frame);
 
-            //Save image
-            String path = output + File.separator + i + ".jpg";
-            ImageIO.write(currentImage, "png", new File(path));
+            for (int w = 0; w < width; w++) {
+
+                for (int h = 0; h < height; h++) {
+
+                    Color baseColor = new Color(baseImage.getRGB(w, h));
+                    int baseBrightness = baseColor.getRed() + baseColor.getGreen() + baseColor.getBlue();
+
+                    int currentValue = currentImage.getRGB(w, h);
+                    Color currentColor = new Color(currentValue);
+                    int currentBrightness = currentColor.getRed() + currentColor.getGreen() + currentColor.getBlue();
+
+                    if (currentBrightness > baseBrightness) {
+                        baseImage.setRGB(w, h, currentValue);
+                        //System.out.println("Changed things");
+                    } else {
+                        //System.out.println("Didn't change things");
+                    }
+
+                    //baseImage.setRGB(w,h,);
+                    //System.out.println("Current image: " + currentImage.getRGB(w, h));
+                }
+            }
 
             System.out.println("Done with frame: " + i);
         }
+
+        //Save image
+        String path = output + File.separator + "output" + ".jpg";
+        ImageIO.write(baseImage, "png", new File(path));
 
         //Close frameGrabber because that's probably good
         frameGrabber.stop();
