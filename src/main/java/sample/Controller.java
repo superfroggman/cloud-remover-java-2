7fe2ad60c445085607b7d0020523feb61af7fa15
linusromland <48645689+linusromland@users.noreply.github.com>
2020-05-25 14:44:14 +0200

    Added nicer graphical, workin filechooser with extensionfilter

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,11 +1,35 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Controller {
+    public static Stage primaryStage;
 
+    @FXML
+    Label selectedfile;
+    @FXML
+    Label title;
+
+    public void initialize() {
+        title.setFont(Font.font("Sans-Serif", 35));
+    }
     public void buttonClicked(ActionEvent actionEvent) throws Exception {
         System.out.println("button clicked");
-        Backend.ffmpegTest();
+        FileChooser _fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Videos (mp4, mkv, mov, gif)", "*.gif", "*.mp4", "*.mkv", "*.mov");
+        _fileChooser.getExtensionFilters().add(extFilter);
+        File file = _fileChooser.showOpenDialog(primaryStage);
+        System.out.println(file.getAbsolutePath());
+        selectedfile.setText("Selected file: " + file.getName());
+
+        //Backend.ffmpegTest();
     }
+
 }
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -4,20 +4,32 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+<<<<<<< Updated upstream
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+=======
+        primaryStage.setTitle("Cloud Remover");
+        primaryStage.setScene(new Scene(root, 500, 375));
+        primaryStage.show();
+        //Backend.customizedCopyPastedThing();
+>>>>>>> Stashed changes
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
+
+
 }
